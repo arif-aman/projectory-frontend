74b81ef55d9e8fe0e3ddc6a0c87be2634abf6650
arif-aman <42302692+arif-aman@users.noreply.github.com>
2021-09-07 21:19:20 +0600

    services edit working

--- src/components/PackageInput.jsx
+++ src/components/PackageInput.jsx
@@ -1,7 +1,6 @@
+import { Box, Button, TextField, Typography } from "@material-ui/core";
 import React from "react";
-import { Box, Typography, TextField, Button } from "@material-ui/core";
 import { AiFillDelete } from "react-icons/ai";
-
 // components
 import RoundedBox from "./RoundedBox";
 
@@ -10,6 +9,8 @@ const PackageInput = ({
   register,
   errorsPrice,
   errorsDeliveryTime,
+  formData,
+  setFormData,
   packageName,
   features,
   setFeatures,
@@ -17,6 +18,31 @@ const PackageInput = ({
   typeFeatures,
   setTypeFeatures,
 }) => {
+  let priceValue;
+  let deliveryValue;
+  let setPrice;
+  let setDelivery;
+
+  if (!register) {
+    if (packageName === "basic") {
+      priceValue = formData.basicPrice;
+      setPrice = (e) => setFormData((prev) => ({ ...prev, basicPrice: e.target.value }));
+      deliveryValue = formData.basicDeliveryTime;
+      setDelivery = (e) => setFormData((prev) => ({ ...prev, basicDeliveryTime: e.target.value }));
+    } else if (packageName === "standard") {
+      priceValue = formData.standardPrice;
+      setPrice = (e) => setFormData((prev) => ({ ...prev, standardPrice: e.target.value }));
+      deliveryValue = formData.standardDeliveryTime;
+      setDelivery = (e) =>
+        setFormData((prev) => ({ ...prev, standardDeliveryTime: e.target.value }));
+    } else if (packageName === "premium") {
+      priceValue = formData.premiumPrice;
+      setPrice = (e) => setFormData((prev) => ({ ...prev, premiumPrice: e.target.value }));
+      deliveryValue = formData.premiumDeliveryTime;
+      setDelivery = (e) =>
+        setFormData((prev) => ({ ...prev, premiumDeliveryTime: e.target.value }));
+    }
+  }
   return (
     <Box width="100%" mt={3}>
       <Box mb={2}>
@@ -25,24 +51,47 @@ const PackageInput = ({
         </Typography>
       </Box>
 
-      <TextField
-        {...register(`${packageName}Price`)}
-        label="Price"
-        helperText={errorsPrice?.message}
-        error={errorsPrice ? true : false}
-        variant="outlined"
-        type="number"
-        className={classes.formInput}
-      />
-      <TextField
-        {...register(`${packageName}DeliveryTime`)}
-        label="Delivery Time (days)"
-        helperText={errorsDeliveryTime?.message}
-        error={errorsDeliveryTime ? true : false}
-        variant="outlined"
-        type="number"
-        className={classes.formInput}
-      />
+      {register ? (
+        <>
+          <TextField
+            {...register(`${packageName}Price`)}
+            label="Price"
+            helperText={errorsPrice?.message}
+            error={!!errorsPrice}
+            variant="outlined"
+            type="number"
+            className={classes.formInput}
+          />
+          <TextField
+            {...register(`${packageName}DeliveryTime`)}
+            label="Delivery Time (days)"
+            helperText={errorsDeliveryTime?.message}
+            error={!!errorsDeliveryTime}
+            variant="outlined"
+            type="number"
+            className={classes.formInput}
+          />
+        </>
+      ) : (
+        <>
+          <TextField
+            label="Price"
+            variant="outlined"
+            type="number"
+            className={classes.formInput}
+            value={priceValue}
+            onChange={setPrice}
+          />
+          <TextField
+            label="Delivery Time (days)"
+            variant="outlined"
+            type="number"
+            className={classes.formInput}
+            value={deliveryValue}
+            onChange={setDelivery}
+          />
+        </>
+      )}
       {/* features */}
       <Box width="100%">
         <Box display="flex" justifyContent="center" alignItems="center">
@@ -51,9 +100,7 @@ const PackageInput = ({
             helperText="5 revision, 3 pages, source code, etc"
             variant="outlined"
             value={features}
-            onChange={(e) =>
-              onChangeFeatureHanlder(packageName, e.target.value)
-            }
+            onChange={(e) => onChangeFeatureHanlder(packageName, e.target.value)}
             className={classes.formInput}
           />
           <Box ml={1} mt={-7}>
@@ -77,16 +124,12 @@ const PackageInput = ({
               <Box
                 key={idx}
                 className={classes.hover}
-                onClick={() =>
-                  setTypeFeatures((prev) =>
-                    prev.filter((item) => item !== feature)
-                  )
-                }
+                onClick={() => setTypeFeatures((prev) => prev.filter((item) => item !== feature))}
               >
                 <RoundedBox
-                  light={true}
-                  icon={true}
-                  borderColor={idx % 2 == 0 ? "primary.main" : "secondary.main"}
+                  light
+                  icon
+                  borderColor={idx % 2 === 0 ? "primary.main" : "secondary.main"}
                 >
                   {feature}
                   <AiFillDelete />
